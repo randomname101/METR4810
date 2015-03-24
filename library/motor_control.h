48050116f9a462adc4a29e35f6df74062f8a5cd6
randomname101 <ihavenoidea11011@gmail.com>
2015-03-24 15:25:37 +1000

    Should be able to power motors with pwm through atmega328 microchip

--- library/motor_control.h
+++ library/motor_control.h
@@ -2,8 +2,8 @@
 #define MOTOR_CONTROL_H
 
 	extern void motor_init(void);
-	extern void power_motor(char id, int duty_cycle);
-	extern int get_motor_power(char id);
+	extern void power_motor(int id, int duty_cycle);
+	extern int get_motor_duty_cycle(int id);
 	extern void emergency_stop(void);
 
 #endif
